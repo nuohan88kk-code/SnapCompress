d1693287e7d68c182c4c3e92c6c8fd2df2af1088
nuohan88kk-code <nuohan88kk@gmail.com>
2026-02-20 18:53:52 +0800

    Update MainActivity.kt

--- T1.0/TT1/SnapCompress/app/src/main/java/com/snapcompress/MainActivity.kt
+++ T1.0/TT1/SnapCompress/app/src/main/java/com/snapcompress/MainActivity.kt
@@ -73,15 +73,11 @@ class MainActivity : ComponentActivity() {
             registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
                 savedUri = null
                 if (uri != null) {
-context.contentResolver.openInputStream(uri).use { input ->
-    if (input != null) {
-        val bmp = BitmapFactory.decodeStream(input, null, BitmapFactory.Options())
-        bitmap = bmp
-        width = bmp?.width?.toFloat()?.coerceAtMost(2000f) ?: 1080f
-    }
-}
-                            }
-                        }
+                context.contentResolver.openInputStream(uri).use { input ->
+                    if (input != null) {
+                        val bmp = BitmapFactory.decodeStream(input, null, BitmapFactory.Options())
+                        bitmap = bmp
+                        width = bmp?.width?.toFloat()?.coerceAtMost(2000f) ?: 1080f
                     }
                 }
             }
