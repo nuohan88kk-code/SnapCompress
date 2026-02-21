4af87c0708ac8a2eb528b6280714cc85a369dffa
nuohan88kk-code <nuohan88kk@gmail.com>
2026-02-21 19:51:29 +0800

    Update SplashActivity.kt

--- T1.0/TT1/SnapCompress/app/src/main/java/com/snapcompress/SplashActivity.kt
+++ T1.0/TT1/SnapCompress/app/src/main/java/com/snapcompress/SplashActivity.kt
@@ -10,9 +10,11 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(this, MainActivity::class))
             finish()
         }, 1200)
     }
 }
+ 
